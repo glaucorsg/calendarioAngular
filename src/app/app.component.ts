7e9a0d7db386d4306bbe4f41882fb670ca2fa4e8
glaucorsg <glaucorsg@gmail.com>
2021-11-14 20:09:52 -0300

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Evento} from "../shared/model/evento";
+import {Usuario} from "../shared/model/usuario";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: "./app.component.html",
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calendario';
+  title = 'Calendário de eventos científicos';
+  usuario: Usuario;
+  usuarios: Array<Usuario>;
+  evento: Evento;
+  eventos: Array<Evento>;
+
+  constructor() {
+    this.usuario = new Usuario();
+    this.usuarios = new Array<Usuario>();
+
+    this.evento = new Evento();
+    this.eventos = new Array<Evento>();
+  }
+
+  inserirUsuario(): void {
+    this.usuarios.push(this.usuario);
+    this.usuario = new Usuario();
+  }
+
+  inserirEvento(): void{
+    this.eventos.push(this.evento);
+    this.evento = new Evento();
+  }
 }
---
+++ src/shared/model/evento.ts
@@ -0,0 +1,6 @@
+export class Evento{
+  nome!:string;
+  data!:string; //possivelmente migrará para tipo data
+  local!:string;
+  tags!:Array<string>;
+}
